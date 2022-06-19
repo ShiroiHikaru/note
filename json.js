b06e9613e4bd836338d4294044dcc082e17914ce
ShiroiHikaru <asd95121@naver.com>
2022-06-19 21:31:08 +0900

    JSON test

---
+++ json.js
@@ -0,0 +1,12 @@
+// Json
+// Javascript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+console.log("hello world");
+// 2. JSON to Object
+// parse(json)
+
