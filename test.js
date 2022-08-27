e2355903705e324c1cdb6cf0a82e90f67df5cc9b
ShiroiHikaru <asd95121@naver.com>
2022-08-27 21:33:07 +0900

    sound clone coding

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-let httpRequest;
-
+function menu(){
+    document.getElementById('mainli').addEventListener('mouseover', function(){
+        document.getElementById('sub').style.display = 'block';
+    }) 
+    document.getElementById('mainli').addEventListener('mouseout', function(){
+        document.getElementById('sub').style.display = 'none';
+    })    
+}
