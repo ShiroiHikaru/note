d7077bd5373d15c726ff4e3bfdc5c4f43f91961b
ShiroiHikaru <asd95121@naver.com>
2022-09-09 20:35:23 +0900

    html 다시 짜보기

--- js/test.js
+++
@@ -1,8 +0,0 @@
-
-    document.getElementById('category').addEventListener('mouseover', function(){
-        document.getElementById('subNav').style.display = 'block';
-    });
-
-    document.getElementById('category').addEventListener('mouseout', function(){
-        document.getElementById('subNav').style.display = 'none';
-    });
